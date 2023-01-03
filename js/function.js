4584b9266c6a50b3bb467e7a0a323885636dd52e
Alan Jojo <alanjojo@proton.me>
2023-01-04 01:14:25 +0530

    Created Posts UI
    To do: Shift GitHub website to GitLab and vice-versa

--- js/function.js
+++ js/function.js
@@ -7,3 +7,31 @@ function menu() {
     if(x.display=="")   x.display = "flex";
     else    x.display = "";
 };
+
+function search_post() {
+    var search_string, items, set;
+    search_string = document.getElementById('search_post').value.toLowerCase();
+    items = document.getElementsByClassName('post-item');
+    for (i = 0; i < items.length; i++) {
+        if (!items[i].getElementsByClassName('post-title')[0].innerHTML.toLowerCase().includes(search_string))
+            items[i].style.display="none";
+        else
+            items[i].style.display="list-item";
+    }
+
+    set = document.getElementsByClassName('posts-group');
+    for (i = 0; i < set.length; i++) { //Each group
+        let x = 0;
+        let group_items = set[i].getElementsByClassName('post-item');
+        for (j = 0; j < group_items.length; j++) {
+            if (group_items[j].style.display!="none") {
+                x = 1;
+                break;
+            }
+        }
+        if (x==0)
+            set[i].style.display="none";
+        else
+            set[i].style.display="block";
+    }
+}
